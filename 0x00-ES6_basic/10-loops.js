888fc9690d3226c6fc031e0fd3769237dbc42e54
alazar-des <alazar.des@gmail.com>
2021-11-26 13:33:59 +0300

    var fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array.keys()) {
-    var value = array[idx];
+    let value = array[idx];
     array[idx] = appendString + value;
   }
 
