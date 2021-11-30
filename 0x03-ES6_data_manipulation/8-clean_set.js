0448fff0b7cb686486c04a67e8d7d796d4290657
alazar-des <alazar.des@gmail.com>
2021-11-30 10:41:01 +0300

    join set starts with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const newSet = new Set();
+  for (const str of set) {
+    if (str.startsWith(startString) && startString !== '') {
+      newSet.add(str.slice(startString.length));
+    }
+  }
+
+  return Array.from(newSet).join('-');
+}
