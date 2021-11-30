9711d7c5aff4465f640b870cd40af72a4cb217a2
alazar-des <alazar.des@gmail.com>
2021-11-30 13:00:15 +0300

    lint fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,7 @@
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      const obj = {
-        status: 200,
-        body: 'success'
-      }
-      return obj
-    }).catch(() => {
-      return Error()
-    }).then((resp) => {
-      console.log('Got a response from the API')
-      return resp
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    })).catch(() => new Error()).then(() => { console.log('Got a response from the API'); });
 }
