26528fb9613bd3037efae65ac6738405069a4bad
alazar-des <alazar.des@gmail.com>
2021-11-30 12:39:49 +0300

    lint fix

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   let arr = [];
-  for (let emp of Object.values(report.allEmployees)) {
+  for (const emp of Object.values(report.allEmployees)) {
     arr = arr.concat(emp);
   }
 
