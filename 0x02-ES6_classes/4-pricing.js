b8266f50718539e6f15b5e96996f1e3eea99961e
alazar-des <alazar.des@gmail.com>
2021-12-03 14:23:26 +0300

    pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+/* eslint-enable no-unused-vars */
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else { throw new TypeError('Amount must be number'); }
+    if (typeof currency === 'object') {
+      this._currency = currency;
+    } else { throw new TypeError('Currency must be a type of Currency'); }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else { throw new TypeError('Amount must be number'); }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (typeof currency === 'object') {
+      this._currency = currency;
+    } else { throw new TypeError('Currency must be a type of Currency'); }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof coversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and Conversion Rate must be numbers');
+  }
+}
