28c6598c232482c25d9837347625245ae6199a84
alazar-des <alazar.des@gmail.com>
2021-12-03 17:11:51 +0300

    abstract method

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else { throw new TypeError('sqft must to number'); }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else { throw new TypeError('sqft must to number'); }
+  }
+
+  evacuationWarningMessage() {
+    throw new
+    Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors === 'number') {
+      this._floors = floors;
+    } else { throw new TypeError('Floors must be number'); }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
