fae93ec95b544316abbdcd07595806a1e5cb5a73
alazar-des <alazar.des@gmail.com>
2021-12-10 14:58:31 +0300

    abstract function

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,18 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["evacuationWarningMessage"] }] */
 export default class Building {
   constructor(sqft) {
     if (typeof sqft === 'number') {
       this._sqft = sqft;
     } else { throw new TypeError('sqft must to number'); }
+
+    const proto = Object.getPrototypeOf(this);
+    const name = 'evacuationWarningMessage';
+    const missing = !Object.prototype.hasOwnProperty.call(proto, name);
+    if (missing) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
   }
 
   get sqft() {
@@ -15,8 +25,5 @@ export default class Building {
     } else { throw new TypeError('sqft must to number'); }
   }
 
-  evacuationWarningMessage() {
-    throw new
-    Error('Class extending Building must override evacuationWarningMessage');
-  }
+  evacuationWarningMessage() {}
 }
