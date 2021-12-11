fa3cb649fbc902d7654c38138fc5250afdd7dd4d
alazar-des <alazar.des@gmail.com>
2021-12-11 16:47:35 +0300

    retune promise of fullfilled arrays

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.resolve(Promise.allSettled(promises)
-    .then((results) => results.forEach((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      }
-      return { status: result.status, value: result.reason };
-    })));
+    .then((results) => results));
 }
