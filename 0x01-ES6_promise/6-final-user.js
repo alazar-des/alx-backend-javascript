1c446f1b8bc6f7d6727d48ba01a9e892fff52542
alazar-des <alazar.des@gmail.com>
2021-12-11 16:04:38 +0300

    after settle all return promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.resolve(Promise.allSettled(promises)
+    .then((results) => results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    })));
+}
