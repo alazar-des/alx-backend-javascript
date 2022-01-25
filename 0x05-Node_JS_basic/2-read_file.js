45b901bca3d8a2cbfc07a9ad9e0036c50aeebbd0
alazar-des <alazar.des@gmail.com>
2022-01-25 12:15:51 +0300

    eslint fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,23 @@
+const fs = require('fs');
+
 function countStudents(path) {
-  const fs = require("fs");
   try {
-    let data = fs.readFileSync(path, "utf8");
-    data = data.split("\n");
+    let data = fs.readFileSync(path, 'utf8');
+    data = data.split('\n');
     let length = data.length - 1;
     if (data[length].length === 0) {
-      --length;
+      length -= 1;
     }
     console.log(`Number of students: ${length}`);
-    let arr = []
-    for (let i = 1; i <= length; i++) {
-      arr.push(data[i].split(","));
+    const arr = [];
+    for (let i = 1; i <= length; i += 1) {
+      arr.push(data[i].split(','));
     }
-    const uniqueFields = [...new Set(arr.map(item => item[3]))];
-    for (let i = 0; i < uniqueFields.length; i++) {
-      const result = arr.filter(items => items[3] === uniqueFields[i]);
-      const names = result.map(name => name[0]).join(', ');
-      console.log(`Number of students in ${uniqueFields[i]}: ${result.length}. List: ${names}`)
+    const uniqueFields = [...new Set(arr.map((item) => item[3]))];
+    for (let i = 0; i < uniqueFields.length; i += 1) {
+      const result = arr.filter((items) => items[3] === uniqueFields[i]);
+      const names = result.map((name) => name[0]).join(', ');
+      console.log(`Number of students in ${uniqueFields[i]}: ${result.length}. List: ${names}`);
     }
   } catch {
     throw new Error('Cannot load the database');
