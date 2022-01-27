4f046464ce38456379ac5bddbbb1ec0644a062ca
alazar-des <alazar.des@gmail.com>
2022-01-27 17:04:02 +0300

    availabel route test

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,49 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('AGI', function () {
+  describe('Index page', function () {
+    it('should return status code of 200', function () {
+      request.get('http://localhost:7865', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
+    });
+    it('should return a body Welcome to the payment system', function () {
+      request.get('http://localhost:7865', function (error, response, body) {
+        expect(body).to.be.equal('Welcome to the payment system');
+      });
+    });
+  });
+
+  describe('Cart Page', function () {
+    it('should return status code of 200', function () {
+      request.get('http://localhost:7865/cart/12', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
+    });
+    it('should return a body Welcome to the payment system', function () {
+      request.get('http://localhost:7865/cart/12', function (error, response, body) {
+        expect(body).to.be.equal('Payment methods for cart 12');
+      });
+    });
+    it('should return status code of 404', function () {
+      request.get('http://localhost:7865/cart/TR', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(404);
+      });
+    });
+  });
+
+    describe('available_payment', function () {
+	it('should return status code of 200', function () {
+	    request.get('http://localhost:7865/available_payments', function (error, response, body) {
+		expect(response.statusCode).to.be.equal(200);
+	    });
+	});
+	it('should return a body Welcome to the payment system', function () {
+	    request.get('http://localhost:7865/available_payments', function (error, response, body) {
+		expect(JSON.parse(body)).to.deep.equal({"payment_methods":{"credit_cards":true,"paypal":false}});
+	    });
+	});
+    });
+});
