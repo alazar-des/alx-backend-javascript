b97f20b785adbe918fbca0ed958dafbf5fadeb7b
alazar-des <alazar.des@gmail.com>
2022-01-27 16:13:24 +0300

    add index test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,20 +2,35 @@ const request = require('request');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('Cart Page', function () {
-  it('should return status code of 200', function () {
-    request.get('http://localhost:7865/cart/12', function (error, response, body) {
-      expect(response.statusCode).to.be.equal(200);
+describe('AGI', function () {
+  describe('Index page', function () {
+    it('should return status code of 200', function () {
+      request.get('http://localhost:7865', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
+    });
+    it('should return a body Welcome to the payment system', function () {
+      request.get('http://localhost:7865', function (error, response, body) {
+        expect(body).to.be.equal('Welcome to the payment system');
+      });
     });
   });
-  it('should return a body Welcome to the payment system', function () {
-    request.get('http://localhost:7865/cart/12', function (error, response, body) {
-      expect(body).to.be.equal('Payment methods for cart :12');
+
+  describe('Cart Page', function () {
+    it('should return status code of 200', function () {
+      request.get('http://localhost:7865/cart/12', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
     });
-  });
-  it('should return status code of 404', function () {
-    request.get('http://localhost:7865/cart/TR', function (error, response, body) {
-      expect(response.statusCode).to.be.equal(404);
+    it('should return a body Welcome to the payment system', function () {
+      request.get('http://localhost:7865/cart/12', function (error, response, body) {
+        expect(body).to.be.equal('Payment methods for cart :12');
+      });
+    });
+    it('should return status code of 404', function () {
+      request.get('http://localhost:7865/cart/TR', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(404);
+      });
     });
   });
 });
