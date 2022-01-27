614169a7a017ed329d03c1119fbfe8c29451a9ab
alazar-des <alazar.des@gmail.com>
2022-01-27 17:28:57 +0300

    test for /login

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('AGI', function () {
-  describe('Index page', function () {
+  describe('/', function () {
     it('should return status code of 200', function () {
       request.get('http://localhost:7865', function (error, response, body) {
         expect(response.statusCode).to.be.equal(200);
@@ -16,7 +16,7 @@ describe('AGI', function () {
     });
   });
 
-  describe('Cart Page', function () {
+  describe('/cart/:id', function () {
     it('should return status code of 200', function () {
       request.get('http://localhost:7865/cart/12', function (error, response, body) {
         expect(response.statusCode).to.be.equal(200);
@@ -34,16 +34,36 @@ describe('AGI', function () {
     });
   });
 
-    describe('available_payment', function () {
-	it('should return status code of 200', function () {
-	    request.get('http://localhost:7865/available_payments', function (error, response, body) {
-		expect(response.statusCode).to.be.equal(200);
-	    });
-	});
-	it('should return a body Welcome to the payment system', function () {
-	    request.get('http://localhost:7865/available_payments', function (error, response, body) {
-		expect(JSON.parse(body)).to.deep.equal({"payment_methods":{"credit_cards":true,"paypal":false}});
-	    });
-	});
+  describe('/available_payment', function () {
+    it('should return status code of 200', function () {
+      request.get('http://localhost:7865/available_payments', function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
     });
+    it('should return a body Welcome to the payment system', function () {
+      request.get('http://localhost:7865/available_payments', function (error, response, body) {
+        expect(JSON.parse(body)).to.deep.equal(
+          { payment_methods: { credit_cards: true, paypal: false } });
+      });
+    });
+  });
+
+  describe('/login', function () {
+    it('should return status code of 200', function () {
+      request.post({
+        url: 'http://localhost:7865/login',
+        form: { userName: 'Betty' }
+      }, function (error, response, body) {
+        expect(response.statusCode).to.be.equal(200);
+      });
+    });
+    it('should return Welcome Betty', function () {
+      request.post({
+        url: 'http://localhost:7865/login',
+        json: { userName: 'Betty' }
+      }, function (error, response, body) {
+        expect(response.body).to.be.equal('Welcome Betty');
+      });
+    });
+  });
 });
