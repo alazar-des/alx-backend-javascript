5229e0dfe426e9cf68e9a0fc990667f4c84ea941
alazar-des <alazar.des@gmail.com>
2022-01-27 16:05:37 +0300

    integration test with route param

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const port = 7865;
+
+app.get('/', function (req, res) {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', function (req, res) {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(404);
+  } else {
+    res.send(`Payment methods for cart :${req.params.id}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Cart Page', function () {
+  it('should return status code of 200', function () {
+    request.get('http://localhost:7865/cart/12', function (error, response, body) {
+      expect(response.statusCode).to.be.equal(200);
+    });
+  });
+  it('should return a body Welcome to the payment system', function () {
+    request.get('http://localhost:7865/cart/12', function (error, response, body) {
+      expect(body).to.be.equal('Payment methods for cart :12');
+    });
+  });
+  it('should return status code of 404', function () {
+    request.get('http://localhost:7865/cart/TR', function (error, response, body) {
+      expect(response.statusCode).to.be.equal(404);
+    });
+  });
+});
