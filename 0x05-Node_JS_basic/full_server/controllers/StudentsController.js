05bd1da94cecaa44a055f65d0ef3de073d286e83
alazar-des <alazar.des@gmail.com>
2022-01-28 14:05:52 +0300

    student controller

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+const readDatabase = require('../utils');
+
+const path = 'database.csv';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    let resp = 'This is the list of our students\n';
+    const arr = [];
+    readDatabase(path)
+      .then((students) => {
+        for (const [key, value] of Object.entries(students)) {
+          arr.push(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+        }
+        resp = resp.concat(arr.join('\n'));
+        response.status(200).send(resp);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    let resp;
+    const { major } = request.params;
+    if (major === 'CS' || major === 'SWE') {
+      readDatabase(path)
+        .then((students) => {
+          for (const [key, value] of Object.entries(students)) {
+            if (major === key) {
+              resp = `List: ${value.join(', ')}`;
+              break;
+            }
+          }
+          response.status(200).send(resp);
+        })
+        .catch(() => {
+          response.status(500).send('Cannot load the database');
+        });
+    } else {
+      response.status(500).send('Major parameter must be CS or SWE');
+    }
+  }
+}
+
+module.exports = StudentsController;
